6a12c09cdc80087d168d60b480ca3437c1a70308
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-10-25 20:56:08 +0300

    fix "Sorry, your reply was invalid: Value is required" on Confirm when validation required is used.

--- validate.go
+++ validate.go
@@ -77,6 +77,14 @@ func isZero(v reflect.Value) bool {
 	switch v.Kind() {
 	case reflect.Slice, reflect.Map:
 		return v.Len() == 0
+		// fixes:
+		// if confirm and `Validate: survey.Required` is used
+		// and answer is "No" (== false)
+		// then it shows "Sorry, your reply was invalid: Value is required"
+		// and it stucks there.
+		// This happens because 'false' is the zero value of a "bool" type.
+	case reflect.Bool: // another solution is just not to use the Required inside Confirms.
+		return false
 	}
 
 	// compare the types directly with more general coverage
